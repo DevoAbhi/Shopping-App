c3eb56999a4c62983dc921fbff47eabb78373591
AbhiDroid <abhinabroy2001@gmail.com>
2021-01-18 03:01:08 +0530

    Extract dynamix id

--- routes/shop.js
+++ routes/shop.js
@@ -3,12 +3,15 @@ const path = require('path');
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+const { route } = require('./admin');
 
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
