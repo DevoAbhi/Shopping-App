3dafa81d0a0a05e663c78159cb89fc87880ad284
AbhiDroid <abhinabroy2001@gmail.com>
2021-01-31 00:15:50 +0530

    Add all necessary routes and admin views

--- helper/database.js
+++ helper/database.js
@@ -3,28 +3,27 @@ const MongoClient = mongodb.MongoClient;
 
 let _db;
 
-
-const mongoConnect = (callback) => {
-    MongoClient.connect('mongodb+srv://Abhinab:GwtyxWee4MWIBfot@cluster.e2ips.mongodb.net/shop?retryWrites=true&w=majority', { useUnifiedTopology: true })
-    .then(result => {
-        console.log("MongoDB Connected!");
-        _db = result.db();
-        callback();
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
     })
     .catch(err => {
-        console.log(err);
-        throw err;
+      console.log(err);
+      throw err;
     });
 };
 
 const getDb = () => {
-    if(_db) {
-        return _db;
-    }
-    else {
-        throw "No Database found!";
-    }
-}
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
