247918bc7aea1bc21c6c0213f7951efd5af03898
AbhiDroid <abhinabroy2001@gmail.com>
2021-01-31 20:24:30 +0530

    Finishing the editing feature

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const mongodb = require('mongodb');
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
@@ -54,14 +54,10 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findById(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
+
+  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, mongodb.ObjectID(prodId));
+
+  product.save()
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
