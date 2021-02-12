57b25ac11e00f0b09eb660d8bc568b2f4c8c2bac
AbhiDroid <abhinabroy2001@gmail.com>
2021-02-13 04:34:18 +0530

    Remove cart and cart-items model

--- models/user.js
+++ models/user.js
@@ -3,14 +3,26 @@ const getDb = require('../helper/database').getDb;
 
 class User{
 
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; //items []
+    this._id = id;
   }
 
   save(){
     const db = getDb();
     return db.collection('users').insertOne(this)
+  }
+
+  addToCart(product){
+
+    const updatedCart = {items: [{ ...product, quantity:1}]}
+    const db = getDb()
+    return db.collection('users')
+    .updateOne({_id: new mongodb.ObjectID(this._id)}, { $set: {cart: updatedCart } }
+    );
+
   }
 
   static findById(userId){
