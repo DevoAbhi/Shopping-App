96383355b21a4d0a15bc03e44b8955ed3671313c
AbhiDroid <abhinabroy2001@gmail.com>
2021-02-15 07:27:12 +0530

    get cart to view

--- models/user.js
+++ models/user.js
@@ -69,12 +69,27 @@ class User{
 
   addOrders(){
     const db = getDb()
-    return db.collection('orders').insertOne(this.cart).then(result => {
+    return this.getCart().then(products => {
+      const order = {
+        items: products,
+        user:{
+          _id: new mongodb.ObjectID(this._id),
+          name: this.name
+        }
+      }
+      return db.collection('orders').insertOne(order)
+    }).then(result => {
       this.cart = {items : []}
       return db.collection('users')
       .updateOne({_id: new mongodb.ObjectID(this._id)}, { $set: {cart: {items : []}  } }
     );
-    })
+    }) 
+  }
+
+  getOrders(){
+    const db = getDb()
+    return db.collection('orders').find({'user._id' : new mongodb.ObjectID(this._id)})
+    .toArray();
   }
 
   static findById(userId){
