2ef398118495d57edb07147a0853cf7b4ff26ef3
AbhiDroid <abhinabroy2001@gmail.com>
2021-02-23 20:17:39 +0530

    Adding Login routes

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
 const Product = require("./models/product");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,6 +32,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(controller404.get404);
 
