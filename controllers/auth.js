3a40f7a2f9dc75aed3f01433835f82dd908b9e9e
AbhiDroid <abhinabroy2001@gmail.com>
2021-03-14 03:34:36 +0530

    making a signup form

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,43 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-    // const isLoggedIn = req.get('Cookie').trim().split('=')[1]
-    res.render('auth/login', {
-        path: '/login',
-        pageTitle: 'Login',
-        isAuthenticated : req.session.isLoggedIn
-    })
-}
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
 
 exports.postLogin = (req, res, next) => {
-    User.findById("6033ceedb9af1b17dceeefa2")
-    .then((user) => {
-        req.session.isLoggedIn = true
-        req.session.user = user;
-        res.redirect('/');   
-    })
-    .catch((err) => console.log(err));
-    
-}
-
-exports.postLogout = (req, res, next) => {
-    req.session.destroy((err) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
         if(err){
             console.log(err);
         }
         res.redirect('/');
+      });
     })
-}
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    if(err){
+        console.log(err);
+    }
+    res.redirect('/');
+  });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,17 @@
 const express = require('express');
+
+const authController = require('../controllers/auth');
+
 const router = express.Router();
-const authController = require('../controllers/auth')
 
-router.get('/login', authController.getLogin)
-router.post('/login', authController.postLogin)
+router.get('/login', authController.getLogin);
 
-router.post('/logout', authController.postLogout)
+router.get('/signup', authController.getSignup);
 
-module.exports = router
+router.post('/login', authController.postLogin);
+
+router.post('/signup', authController.postSignup);
+
+router.post('/logout', authController.postLogout);
+
+module.exports = router;
